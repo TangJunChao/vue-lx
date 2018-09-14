9070670dbc38dbfa4090df96bf35684dc7492a92
TangJunChao <junchaotang@qq.com>
2018-09-14 10:16:47 +0800

    侧边栏提交

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <template>
   <div>
+    <!-- 头部组件 -->
     <my-header></my-header>
     <div class="container-fluid">
       <div class="row">
-        <div class="col-sm-3 col-md-2 sidebar">
-          <ul class="nav nav-sidebar">
-            <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Reports</a></li>
-            <li><a href="#">Analytics</a></li>
-            <li><a href="#">Export</a></li>
-          </ul>
-        </div>
+        <!-- 侧边栏 -->
+        <my-sidebar></my-sidebar>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
           <h2 class="sub-header">Hero List</h2>
           <a class="btn btn-success" href="add.html">Add</a>
@@ -230,10 +225,12 @@
 
 <script>
 import MyHeader from '@/components/myheader.vue';
+import MySidebar from '@/components/mysidebar.vue';
 export default {
   name: 'App',
   components: {
-    MyHeader
+    MyHeader,
+    MySidebar
   }
 };
 </script>
