af3bbe5256f72690e765b567c2b470048a19eae6
TangJunChao <junchaotang@qq.com>
2018-09-14 11:16:07 +0800

    英雄列表

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+// 加载路由规则对应的组件
+import HeroesList from '@/views/heroes/list';
+
 // 注册Vue-Router插件
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'HelloWorld',
-    //   component: HelloWorld
-    // }
+    {name: 'home', path: '/', redirect: '/heroes'},
+    {
+      path: '/heroes',
+      name: 'heroes',
+      component: HeroesList
+    }
   ]
 });
